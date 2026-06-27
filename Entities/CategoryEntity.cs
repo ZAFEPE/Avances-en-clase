a6e76321ecfb7658e598d84afa001333790b1523
ZAFEPE <zahirperdomo0@gmail.com>
2026-06-27 17:07:58 -0600

    Revisa en las partes que hay que cambiar el codigo del puerto al tuyo

---
+++ Entities/CategoryEntity.cs
@@ -0,0 +1,14 @@
+namespace Sales.API.Entities
+{
+    public class CategoryEntity
+    {
+        public int Id {get; set;}
+        public string? Name{get; set;}
+        public string? Description { get; set; }
+        //criao por
+        //fecha de criacion
+        // ultimo en editar
+        //fecha diultima edicion
+        
+    }
+}
